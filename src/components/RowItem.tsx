ec25e1622ff0a969f81921e83273df2cf91581c1
qasimfida <qasimfida0054@gmail.com>
2025-02-10 22:11:26 -1200

    mobile responsive

--- src/components/RowItem.tsx
+++ src/components/RowItem.tsx
@@ -25,21 +25,22 @@ const RowItem: React.FC<RowItemProps> = ({
   handleClick,
 }) => {
   return (
-   <>
-    <div
-      className={`md:flex grid text-sm md:text-md font-medium mt-[12px]  grid-cols-[40px,1fr] grid-rows-[auto,auto,auto] text-${textColor}  `}
-      onClick={handleClick}
-      key={key}
-    >
-      <p className="md:mr-[7px] row-start-1 col-start-1 ">{sno + ")"}</p>
-      <p className={`md:w-[84%] row-start-1 col-start-2 text-${paraColor}`}>{text}</p>
-      <div className="md:w-[15%] col-span-2 flex justify-between">
-        <p className={`text-red  text-${bnColor}`}>{bn}</p>
-        <p className={`text-yellow text-${timeColor}`}>{time}</p>
+    <>
+      <div
+        className={`md:flex hover:border hover:border-white grid text-sm md:text-md font-medium py-[6px]  grid-cols-[40px,1fr] grid-rows-[auto,auto,auto] text-${textColor}  `}
+        onClick={handleClick}
+        key={key}
+      >
+        <p className="md:mr-[7px] row-start-1 col-start-1 ">{sno + ")"}</p>
+        <p className={`md:w-[84%] row-start-1 col-start-2 text-${paraColor}`}>
+          {text}
+        </p>
+        <div className="md:w-[15%] col-span-2 flex justify-between">
+          <p className={`text-red  text-${bnColor}`}>{bn}</p>
+          <p className={`text-yellow text-${timeColor}`}>{time}</p>
+        </div>
       </div>
-    </div>
     </>
-    
   );
 };
 export default RowItem;
