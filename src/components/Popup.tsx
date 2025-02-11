b916c72813b067650d510c47b31e738057a2540f
qasimfida <qasimfida0054@gmail.com>
2025-02-10 17:48:07 -1200

    mobile responsive

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -26,10 +26,14 @@ const Popup: React.FC<PopupProps> = ({ title, content, onClose }) => {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div
         ref={popupRef}
-        className="bg-[#1E1E1E] text-white border-[2px] border-white shadow-2xl py-[32px] px-[60px] w-4/5 max-w-4xl relative rounded-lg"
+        className="bg-[#1E1E1E] text-white border-[2px] border-white shadow-2xl py-[16px] px-[12px] lg:py-[32px] lg:px-[60px] w-4/5 max-sm:w-[90%] max-w-4xl relative "
       >
-        <h2 className="text-[32px] font-medium mb-[30px]">{title}</h2>
-        <p className="text-[20px] leading-relaxed text-[#E0AB74]">{content}</p>
+        <h2 className="md:text-[32px] text-[16px] font-medium mb-[10px] lg:mb-[30px]">
+          {title}
+        </h2>
+        <p className="md:text-[20px] text-[12px] leading-relaxed text-[#E0AB74]">
+          {content}
+        </p>
       </div>
     </div>
   );
